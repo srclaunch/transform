2b69fc31c939f7e201cc5f8796e24bf62a1d6642
Steven Bennett <steven@budgetbloom.com>
2022-04-25 16:13:12 -0700

    Quick release - patch

---
+++ dist/index.mjs
@@ -0,0 +1,13 @@
+function formatObjectToCSVData({
+  objectType
+}) {
+  switch (objectType) {
+    case "something":
+      return "";
+    default:
+      return "";
+  }
+}
+
+export { formatObjectToCSVData };
+//# sourceMappingURL=index.mjs.map
