f98bf67adb3838713e73311ace75620703f701f2
Steven Bennett <steven@budgetbloom.com>
2022-04-29 19:09:28 -0700

    Clean cache

--- dist/index.umd.js
+++
@@ -1,2 +0,0 @@
-(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e["@srclaunch/transform"]={}))})(this,function(e){"use strict";function t({objectType:n}){switch(n){case"something":return"";default:return""}}e.formatObjectToCSVData=t,Object.defineProperties(e,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}})});
-//# sourceMappingURL=index.umd.js.map
